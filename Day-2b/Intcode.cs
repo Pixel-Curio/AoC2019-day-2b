05796d66166f50f3d82d9715bb5445642b2bc77b
Michael Sanders <mike.gamedev@gmail.com>
2019-12-05 23:15:13 -0800

    Initial commit.

---
+++ Day-2b/Intcode.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Day_2b
+{
+    class Intcode
+    {
+        private int _pointer;
+        private readonly int[] _code;
+
+        public Intcode(int[] code) => _code = code;
+
+        public void Process()
+        {
+            while (true)
+            {
+                int command = ConsumeOpCode();
+                int a, b, t;
+                switch (command)
+                {
+                    case 1:
+                        a = ConsumeOpCode();
+                        b = ConsumeOpCode();
+                        t = ConsumeOpCode();
+                        _code.ExpandingSet(t, _code.ExpandingGet(a) + _code.ExpandingGet(b));
+                        //_code[t] = _code[a] + _code[b];
+                        break;
+                    case 2:
+                        a = ConsumeOpCode();
+                        b = ConsumeOpCode();
+                        t = ConsumeOpCode();
+                        _code.ExpandingSet(t, _code.ExpandingGet(a) * _code.ExpandingGet(b));
+                        //_code[t] = _code[a] * _code[b];
+                        break;
+                    case 99:
+                        return;
+                }
+            }
+        }
+
+        public int PrintValue(int index) => _code[index];
+
+        private int ConsumeOpCode() => _pointer < _code.Length ? _code[_pointer++] : 99;
+    }
+
+    public static class ArrayExtensions
+    {
+        public static void ExpandingSet(this int[] source, int index, int value)
+        {
+            if (index > source.Length) Array.Resize(ref source, index + 1);
+            source[index] = value;
+        }
+        public static int ExpandingGet(this int[] source, int index)
+        {
+            if (index > source.Length) Array.Resize(ref source, index + 1);
+            return source[index];
+        }
+    }
+}
---
+++ Day-2b/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day_2b
+{
+    class Program
+    {
+        private const string InputPath = @"day2a-input.txt";
+        private const int TargetValue = 19690720;
+
+        static void Main(string[] args)
+        {
+            int[] code = File.ReadAllText(InputPath).Split(",").Select(int.Parse).ToArray();
+
+            for (int x = 0; x < 99; x++)
+            {
+                for (int y = 0; y < 99; y++)
+                {
+                    code = File.ReadAllText(InputPath).Split(",").Select(int.Parse).ToArray();
+                    code[1] = x;
+                    code[2] = y;
+
+                    Intcode processor = new Intcode(code);
+                    processor.Process();
+
+                    Console.WriteLine($"Processing X:{x} Y:{y} Result:{processor.PrintValue(0)}");
+
+                    if (processor.PrintValue(0) == TargetValue)
+                    {
+                        Console.WriteLine($"Success Value = X:{x},Y:{y}");
+                        return;
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
